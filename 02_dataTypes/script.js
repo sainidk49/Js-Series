ac54399b984c498789675cff9761604da5a43be8
Deepakkk Saini <dpksaini49@gmail.com>
2024-06-21 12:19:21 +0530

    Add DataType file

---
+++ 02_dataTypes/script.js
@@ -0,0 +1,13 @@
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// =========== typeof ===========
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
