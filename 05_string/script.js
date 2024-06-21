e42c1ff6c5addb0e30e2f178a2f9d1a98eddb005
Deepakkk Saini <dpksaini49@gmail.com>
2024-06-21 17:33:48 +0530

    Add new file of string

---
+++ 05_string/script.js
@@ -0,0 +1,19 @@
+// let userName = new String()
+let userName = "deepak saini"
+// userName = userName.length // => provide length of string including space
+
+// userName = userName.charAt(2) // => provide character through the index value
+
+// userName = userName.indexOf("p") // => provide index through the key value
+
+// userName = userName.split(" ") // => convert into array :- ["deepak", "saini"]
+
+// userName = userName.substring(0, 4) // => provide word index 0 to 4 : "deep"
+
+// let userName = "  deepak saini  "
+// userName = userName.trim() // =>  it remove white space from start and End 
+
+// let userName = "deepak=saini"
+// userName = userName.replace("=", " ") // =>  it replace the value 
+
+console.log(typeof userName)
