6037b60305a18387d9424070537cb1b7fd6049a4
Deepakkk Saini <dpksaini49@gmail.com>
2024-06-21 15:39:42 +0530

    add premitive and non-premitive data type in another file

---
+++ 03_typeof/script.js
@@ -0,0 +1,47 @@
+// let value = 33
+// let value = "33"
+// let value = "33abc"
+// let value = true;
+// let value = false;
+// let value;
+// console.log("value : " + value)
+// console.log("typeof : "+ typeof value)
+
+// console.log("=================")
+
+// let IntValue = Number(value);
+// console.log("value : " + IntValue)
+// console.log("typeof : "+ typeof IntValue)
+
+
+
+// ////////////////// conver a number into string //////////////////////
+// let value = 33
+// let strValue = String(value)
+// console.log("strValue : " +  typeof strValue)
+
+
+//////////////////// convert a number and string into boolean ////////////
+// let value = 0; //=> it retuns false
+// let value = 1; //=> it retuns true
+// let value = ""; //=> it retuns false
+// let value = "dk saini"; //=> it retuns true
+// let blnValue = Boolean(value)
+// console.log(blnValue);
+
+
+// ===================== Operations ===========================
+
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// console.log("==================")
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4" 
---
+++ 04_premitive_NonPremitive/script.js
@@ -0,0 +1,44 @@
+// ========== Premitive Data type ==========
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// let id = Symbol("123")  //=> return "Symbol('123')"
+// let uniqueId = Symbol("123") //=> return "Symbol('123')"
+// console.log(uniqueId === id) //=> return "false"
+
+
+// ========== Reference => Non-Premitive Data type ==========
+// 3 types : Array, Objects, Functions
+
+// let listArr = ["Dk", "Ajay", "Yash", "Raj"]
+
+// let user = {
+//     name: "deepak",
+//     age: 26,
+//     email: "dpksaini49@gmail.com"
+// }
+
+// let fun = function(){
+//     console.log("hello world")
+// }
+
+// console.log(typeof listArr) // => object
+// console.log(typeof user) // => object
+// console.log(typeof fun) // => function
+
+
+/// ================== Stack (Premitive) and Heap(Non-Premitive)
+
+// let value1 = "dksaini"
+// let value2 = value1;
+// value2 = "dpksaini"
+// console.log(`value1: ${value1} :: value2 ${value2}`) // => its make a copy or variable in Stack
+
+// let user1 = {
+//     name : "deepak",
+//     age: 26
+// }
+// let user2 = user1
+// user2.name = "dksaini";
+// user2.age = 27;
+// console.log(user1) // => its make the change in original object
+// console.log(user2) // => return same value
