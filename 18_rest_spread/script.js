019b25f96a3f6997bc19319ef8e3718a5f93bfb0
Deepak Saini <dpksaini49@gmail.com>
2024-11-27 12:26:32 +0530

    for the practice

--- 18_rest_spread/script.js
+++ 18_rest_spread/script.js
@@ -4,7 +4,6 @@ let numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 // function resSpreadNum(a, b, c, ...all) {
 //     console.log(a + b + c)
 //     return all
-
 // }
 // console.log(resSpreadNum(...numArr))
 
